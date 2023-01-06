6e56a32f08a941df96c23ea81af1ec31fbc212e4
adairbeck <113452807+adairbeck@users.noreply.github.com>
2023-01-06 09:48:42 -0500

    Update sketch.js

--- CreativeCoding/Grid/sketch.js
+++ CreativeCoding/Grid/sketch.js
@@ -1,10 +1,11 @@
 function setup() {//run once
 createCanvas(720,720);//creates a canvas 720x720 pixels
+background(92, 114, 255);
 }
 
 function draw() {//runs in a loop
 fill(185, 169, 245);//fills shape with rgb color
-strokeWeight(8);//assigns stroke weight 
+strokeWeight(8);//assigns stroke weight
 quad (0,0,
       300,0,
       300,300,
