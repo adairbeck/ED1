2c19f38eaffca49992498db2dfc06861904a5074
adairbeck <113452807+adairbeck@users.noreply.github.com>
2023-01-06 09:54:23 -0500

    Update sketch.js

--- CreativeCoding/Grid/sketch.js
+++ CreativeCoding/Grid/sketch.js
@@ -6,8 +6,24 @@ background(92, 114, 255);
 function draw() {//runs in a loop
 fill(185, 169, 245);//fills shape with rgb color
 strokeWeight(8);//assigns stroke weight
+translate(60,60);//creates a new origin point 60,60
 quad (0,0,
       300,0,
       300,300,
       0,300);
+
+quad (300,0,
+      600,0,
+      600,300,
+      300,300);//add 300 in x direction
+
+quad (0,300,
+      300,300,
+      300,600,
+      0,600);
+
+      quad (300,300,
+            600,300,
+            600,600,
+            300,600);//add 300 in x direction
 }
