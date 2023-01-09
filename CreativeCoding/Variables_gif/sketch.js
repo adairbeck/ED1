451f8b95852e3b28bdc57b2822fb1615e61545f6
adairbeck <113452807+adairbeck@users.noreply.github.com>
2023-01-09 13:16:39 -0500

    ok

--- CreativeCoding/Variables_gif/sketch.js
+++ CreativeCoding/Variables_gif/sketch.js
@@ -1,11 +1,16 @@
 //create a program that enables us to customize the number of squares in a grid
+let img;
+
+function preload (){
+  gif = loadImage ('assets/AdairGraphicWeb.ai')
+}
 
 function setup() {//run once
 createCanvas(windowWidth,windowWidth);//creates a canvas 720x720 pixels
 }
 
 function draw() {//runs in a loop
-fill(185, 169, 245);//fills shape with rgb color
+fill('fuchsia');//fills shape with rgb color
 strokeWeight(1);//assigns stroke weight
 
 var num =2; //number of sides in the array
@@ -13,11 +18,8 @@ var sideLen = windowWidth/num ;//side length variable
 
 for (var y = 0; y < windowWidth; y = y+sideLen){
   for (var x = 0; x < windowWidth; x = x+sideLen){
-    quad (x,y,
-          x+sideLen,y,
-          x+sideLen,sideLen+y,
-          x,sideLen+y);
 
+image (img, x, y, windowWidth/num; windowWidth/num);
   }
 
 }
