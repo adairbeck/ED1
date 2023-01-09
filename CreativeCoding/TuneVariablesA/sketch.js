3e2e279bdc2042764478c0d60efda087e4f1c53e
adairbeck <113452807+adairbeck@users.noreply.github.com>
2023-01-09 12:44:23 -0500

    Update sketch.js

--- CreativeCoding/TuneVariablesA/sketch.js
+++ CreativeCoding/TuneVariablesA/sketch.js
@@ -1,20 +1,19 @@
+//create a program that enables us to customize the number of squares in a grid
+
 function setup() {//run once
 createCanvas(720,720);//creates a canvas 720x720 pixels
-background(179, 255, 237);
 }
 
 function draw() {//runs in a loop
 fill(185, 169, 245);//fills shape with rgb color
-strokeWeight(8);//assigns stroke weight
-translate(60,60);//creates a new origin point 60,60
+strokeWeight(1);//assigns stroke weight
 
-for (var y = 0; y < 600; y = y+300){
-
-  for (var x = 0; x < 600; x = x+300){
+for (var y = 0; y < 720; y = y+360){
+  for (var x = 0; x < 720; x = x+360){
     quad (x,y,
-          x+300,y,
-          x+300,300+y,
-          x,300+y);
+          x+360,y,
+          x+360,360+y,
+          x,360+y);
 
   }
 
