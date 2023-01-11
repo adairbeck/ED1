bae872cd00d4675414a938246337bd369dd28a06
adairbeck <113452807+adairbeck@users.noreply.github.com>
2023-01-11 09:23:53 -0500

    update

--- CreativeCoding/TuneVariablesA/sketch.js
+++ CreativeCoding/TuneVariablesA/sketch.js
@@ -8,7 +8,7 @@ function draw() {//runs in a loop
 fill(185, 169, 245);//fills shape with rgb color
 strokeWeight(1);//assigns stroke weight
 
-var num =2; //number of sides in the array
+var num =30; //number of sides in the array
 var sideLen = windowWidth/num ;//side length variable
 
 for (var y = 0; y < windowWidth; y = y+sideLen){
