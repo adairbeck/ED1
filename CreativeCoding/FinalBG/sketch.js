2683a87b7104d0a08b569f3392cad7938d7ffc08
adairbeck <113452807+adairbeck@users.noreply.github.com>
2023-01-11 15:41:40 -0500

    update

--- CreativeCoding/FinalBG/sketch.js
+++ CreativeCoding/FinalBG/sketch.js
@@ -14,7 +14,7 @@ function draw() {//runs in a loop
 fill ('yellow');//fills shape with RGB color
 strokeWeight (1);//assigns stroke weight
 
-var num = 30; //number of sides in the array
+var num = 8; //number of sides in the array
 var sideLen = windowWidth/num; //side length variable
 
 translate (-150, -150);//this will give your design a bleed effect
