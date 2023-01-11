8d3318afcbc4ccfef3433fb4463a84bfa1c65237
adairbeck <113452807+adairbeck@users.noreply.github.com>
2023-01-11 15:37:19 -0500

    Update sketch.js

--- CreativeCoding/FinalBG/sketch.js
+++ CreativeCoding/FinalBG/sketch.js
@@ -1,34 +1,31 @@
-//create a program that enables us to customize the number of squares in a grid
-let img;
+//create a program that enables up to customize the number of squares in a grid
+let img;//"let img;" for image
 
 function preload (){
-  gif = loadImage ('assets/ScreenShot.png')
+  img = loadImage ('assets/ScreenShot.png');
 }
 
-function setup() {//run once
-createCanvas(windowWidth,windowWidth);//creates a canvas 720x720 pixels
+function setup() {//runs once
+createCanvas (windowWidth, windowWidth);// creates a canvas 720 pixels by 720 pixels
+//background ('maroon'); //background color
 }
 
 function draw() {//runs in a loop
-fill('fuchsia');//fills shape with rgb color
-strokeWeight(1);//assigns stroke weight
+fill ('yellow');//fills shape with RGB color
+strokeWeight (1);//assigns stroke weight
 
-var num =5; //number of sides in the array
-var sideLen = windowWidth/num ;//side length variable
+var num = 30; //number of sides in the array
+var sideLen = windowWidth/num; //side length variable
 
-translate (-150, -150);
+translate (-150, -150);//this will give your design a bleed effect
 
-for (var y = 0; y < 2 * windowWidth; y = y+sideLen){
-  for (var x = 0; x < 2 * windowWidth; x = x+sideLen){
-
-image (img, x, y, windowWidth/num; windowWidth/num);
+for (var y = 0; y < 2 * windowWidth; y = y + sideLen){
+  for (var x = 0; x < 2 * windowWidth; x = x + sideLen) {
+image (img, x, y, windowWidth/num, windowWidth/num);//img for image
   }
-
+}
 }
 
-}
-
-}
-function windowResized () {//allow windowrefresh with window resizing
+function windowResized (){//allow window refresh with window resizing
   resizeCanvas (windowWidth, windowHeight);
 }
